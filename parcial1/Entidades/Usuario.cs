5140184e76b3a3dd3c9d7cacc6ddab8d7bdba0fd
Jas Menjivar <mm0588032021@unab.edu.sv>
2021-09-13 16:41:09 -0600

    parcial

---
+++ parcial1/Entidades/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parcial1.Entidades
+{
+    class Usuario
+    {
+        private string nombre;
+        private int contraseña;
+
+        public Usuario(string nombre, int contraseña)
+        {
+            this.Nombre = nombre;
+            this.Contraseña = contraseña;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Contraseña { get => contraseña; set => contraseña = value; }
+    }
+}
